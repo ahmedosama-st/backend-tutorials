c49f18ce4b2480dd379121ded135e19b30af53c1
Ahmed Osama <marvelphp5@gmail.com>
2021-06-25 13:41:31 +0200

    Removed drawio

--- sql/coding-exercise-3/ex-fixed.sql
+++ sql/coding-exercise-3/ex-fixed.sql
@@ -1,17 +1,38 @@
-SELECT DISTINCT
-	s1.id,
+SELECT
+	DISTINCT s1.id,
 	s1.visit_date,
-	s1.people 
+	s1.people
 FROM
-	( SELECT * FROM stadium WHERE people >= 100 ) s1,
-	( SELECT * FROM stadium WHERE people >= 100 ) s2,
-	( SELECT * FROM stadium WHERE people >= 100 ) s3 
+	(
+		SELECT
+			*
+		FROM
+			stadium
+		WHERE
+			people >= 100
+	) s1,
+	(
+		SELECT
+			*
+		FROM
+			stadium
+		WHERE
+			people >= 100
+	) s2,
+	(
+		SELECT
+			*
+		FROM
+			stadium
+		WHERE
+			people >= 100
+	) s3
 WHERE
-	s1.id = s2.id - 1 
-	AND s2.id = s3.id - 1 
-	OR s2.id = s1.id - 1 
-	AND s1.id = s3.id - 1 
-	OR s3.id = s2.id - 1 
-	AND s2.id = s1.id - 1 
+	s1.id = s2.id - 1
+	AND s2.id = s3.id - 1
+	OR s2.id = s1.id - 1
+	AND s1.id = s3.id - 1
+	OR s3.id = s2.id - 1
+	AND s2.id = s1.id - 1
 ORDER BY
 	s1.visit_date;
--- sql/coding-exercise-4/ex-fixed.sql
+++ sql/coding-exercise-4/ex-fixed.sql
@@ -1,21 +1,45 @@
 SELECT
 	id,
 	visit_date,
-	people 
+	people
 FROM
 	(
-	SELECT
-		id,
-		visit_date,
-		people,
-		lead( people, 1 ) over ( ORDER BY visit_date ) AS two,
-		lead( people, 2 ) over ( ORDER BY visit_date ) AS three,
-		lag( people, 1 ) over ( ORDER BY visit_date ) AS four,
-		lag( people, 2 ) over ( ORDER BY visit_date ) AS five 
-	FROM
-		stadium 
-	) a 
+		SELECT
+			id,
+			visit_date,
+			people,
+			lead(people, 1) over (
+				ORDER BY
+					visit_date
+			) AS two,
+			lead(people, 2) over (
+				ORDER BY
+					visit_date
+			) AS three,
+			lag(people, 1) over (
+				ORDER BY
+					visit_date
+			) AS four,
+			lag(people, 2) over (
+				ORDER BY
+					visit_date
+			) AS five
+		FROM
+			stadium
+	) a
 WHERE
-	( a.people >= 100 AND a.two >= 100 AND a.three >= 100 ) 
-	OR ( a.people >= 100 AND a.four >= 100 AND a.five >= 100 ) 
-	OR ( a.people >= 100 AND a.two >= 100 AND a.four >= 100 );
+	(
+		a.people >= 100
+		AND a.two >= 100
+		AND a.three >= 100
+	)
+	OR (
+		a.people >= 100
+		AND a.four >= 100
+		AND a.five >= 100
+	)
+	OR (
+		a.people >= 100
+		AND a.two >= 100
+		AND a.four >= 100
+	);
