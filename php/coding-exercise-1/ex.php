fbd60bdf8d85fb5960062c66b0c89ad5371ac3bb
Ahmed Osama <marvelphp5@gmail.com>
2021-07-17 14:31:04 +0200

    Added test cases for either coding exercise

--- php/coding-exercise-1/ex.php
+++ php/coding-exercise-1/ex.php
@@ -3,3 +3,10 @@
 function either()
 {
 }
+
+var_dump(either('', 0, -0, [], [1]) == [1]); // true
+var_dump(either("ahmed", '0', '-0', [1,2,3,4], 'ahmed') == 'ahmed'); // true
+var_dump(either('0', '-0', 'ahmed') == '-0'); // true
+var_dump(either('', 0, -0, []) == NULL); // true
+var_dump(either([[]], 1, 2, -3, -4) == [[]]); // true
+var_dump(either([], 0, '', -1) == -1); // true
