0e33c49496ec726976fdd1076a56e45ffe78fa0b
Ahmed Osama <marvelphp5@gmail.com>
2021-07-31 20:16:18 +0200

    Added test cases for array_combine coding exercise

--- php/coding-exercise-2-array_combine-function/ex.php
+++ php/coding-exercise-2-array_combine-function/ex.php
@@ -4,3 +4,7 @@ function arrayCombine($keys, $values)
 {
     // implement your logic here
 }
+
+var_dump(arrayCombine(['one', 'two'], [1, 2]) == ['one' => 1, 'two' => 2]);
+var_dump(arrayCombine(['one', 'two', 'three'], [1, 2]) == false);
+var_dump(arrayCombine([], []) == []);
